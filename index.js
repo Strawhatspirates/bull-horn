738bae1a56a4d51dd522c9bf699215c9e203e01e
plasmashadow <plasmashadowx@gmail.com>
2016-07-06 13:10:53 +0530

    changed the api

--- index.js
+++ index.js
@@ -1,53 +1,52 @@
-module.exports = BullHorn = (function(){
+module.exports = global.BullHorn = function(sampling) {
 
-   //initializing the mutation summary
-   var Summary = require('./lib/summary');
-   var Events = require('minivents');
-   var Q = require('q');
-   var assign = require('object-assign');
+    //initializing the mutation summary
+    var Summary = require('./lib/summary.js');
+    var Events = require('minivents');
+    var assign = require('object-assign');
 
-   var self = this;
-   var _module = new Object();
+    var self = this;
+    var _module = new Object();
 
-   var globalWorker = Q.defer();
-   var globalData = null;
+    var globalData = null;
 
-   var Observer = new Summary();
+    var Observer = global._observer = new Summary(sampling);
 
-   //inherit the eventing library
-   assign(_module, new Events());
+    //inherit the eventing library
+    assign(_module, new Events());
 
 
-   //maintating the global list of events
-   var _events = [
-     "onclick",
-     "onkeypress",
-     "onkeydown"
-   ];
+    //maintating the global list of events
+    var _events = [
+        "onclick",
+        "onkeypress",
+        "onkeydown"
+    ];
 
-   //binding global listeners
-   function initialize(){
-     for(var key in _events){
-       var event = _events[key];
-       document[event] = onObserve;
-     }
-   }
+    //binding global listeners
+    function initialize() {
+        for (var key in _events) {
+            var event = _events[key];
+            document[event] = onObserve;
+        }
+    }
 
-   function onObserve(e){
-     var data = {
-       type: e.type,
-       x: e.x,
-       y: e.y,
-       created: new Date().getTime(),
-       element: {
-         id: e.target.id,
-         tag: e.target.tag,
-         text: e.target.innerHTML
-       },
-       summaries: Observer.getRecentSummaries(5)
-     };
-     _module.emit('observation', data);
-   }
+    function onObserve(e) {
+        var data = {
+            type: e.type,
+            x: e.x,
+            y: e.y,
+            created: new Date().getTime(),
+            element: {
+                id: e.target.id,
+                tag: e.target.tag,
+                text: e.target.innerHTML
+            },
+            summaries: Observer.getRecentSummaries(5)
+        };
+        _module.emit('observation', data);
+    }
+    initialize();
+    return _module;
 
-
-})();
+}
--- test/bull-spec.js
+++ test/bull-spec.js
@@ -1,10 +1,28 @@
 describe("bull-horn-test-suite", function() {
 
-   beforeEach(function(){
+    beforeEach(function() {
+        this.document = $(document);
+        this.horn = new BullHorn(2000);
+        this.textField = $("<input></input>", {
+          type: "text",
+          id: "hello"
+        })
+    });
 
-   });
+    it('should observe for observation event', function(done) {
+        var deferred = $.Deferred();
+        this.horn.on('observation', function(data) {
+            console.log(data);
+            deferred.resolve(data);
+        });
+        deferred.done(function() {
+            done();
+        });
+        this.document.click();
+    });
 
-   it('should observe for observation event', function(done){
+    // it('should detect keypress event', function(done) {
+    //   done();
+    // });
 
-   });
 });
